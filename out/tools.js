56c99caa782ff1c525efe4febbd360b20beac94e
Excd <greggseamon@gmail.com>
2023-08-22 06:23:19 -0400

    Create getMaxThreads lib function
    Update comments and help message.

--- out/tools.js
+++ out/tools.js
@@ -13,6 +13,8 @@ export function main(ns) {
   // Help message.
   if (flags.help) {
     ns.tprint(
+      'Allows terminal usage of library functions via arguments (case sensitive). Alternative' +
+        ' Netscript functions require the ns flag (--ns).' +
         `\nScript Usage: > run ${ns.getScriptName()} <--ns> {command} <arg1 arg2...>` +
         `\n     Example: > run ${ns.getScriptName()} getServers server-term home` +
         `\n     Example: > run ${ns.getScriptName()} --ns getPurchasedServers`
@@ -36,6 +38,7 @@ export function main(ns) {
     return;
   }
 
+  // Print formatted result.
   ns.tprint(Array.isArray(result) ? `\n${result.join('\n')}` : result);
 }
 
@@ -44,9 +47,23 @@ export function main(ns) {
  */
 export const lib = {
   /**
+   * Returns the maximum threads usable by a script given available server RAM and
+   * script requirement.
+   * @remarks RAM cost: 0 GB
+   * @param {number} available - Available server RAM.
+   * @param {number} required - Script RAM requirement.
+   * @returns {number} Maximum usable threads.
+   **/
+  getMaxThreads: function (available, required) {
+    return Math.floor(available / required);
+  },
+
+  /**
    * Returns an array of servers.
    * @remarks RAM cost: 0.2GB
    * @param {import('@ns').NS} ns - Netscript environment.
+   * @param {string} [term] - Optional. Search term for server names.
+   * @param {string} [hostname=home] - Optional. Hostname of server to scan.
    * @returns {string[]} Array of servers.
    */
   getServers: function (ns, term = '', hostname = 'home') {
@@ -62,6 +79,7 @@ export const nslib = {
    * Returns an array of purchased servers. Cheaper alternative to ns.getPurchasedServers().
    * @remarks RAM cost: 0GB
    * @param {import('@ns').NS} ns - Netscript environment.
+   * @param {string} [term=pserv] - Optional. Search term for server names.
    * @returns {string[]} Array of purchased servers.
    */
   _getPurchasedServers: function (ns, term = 'pserv') {
