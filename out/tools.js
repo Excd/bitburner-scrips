add15daf8cfc6ad724b0cf2871a501aec5dbef7e
Excd <greggseamon@gmail.com>
2023-08-22 06:21:38 -0400

    Move tools.js up a directory

--- out/tools.js
+++ out/tools.js
@@ -1,6 +1,5 @@
 /**
  * Main method. Allows terminal usage of library functions via arguments.
- * @author excd
  * @remarks RAM cost: 1.8GB
  * @param {import('@ns').NS} ns - Netscript environment.
  */
@@ -14,8 +13,6 @@ export function main(ns) {
   // Help message.
   if (flags.help) {
     ns.tprint(
-      'Allows terminal usage of library functions via arguments. Alternative Netscript' +
-        ' functions require the ns flag (--ns).' +
         `\nScript Usage: > run ${ns.getScriptName()} <--ns> {command} <arg1 arg2...>` +
         `\n     Example: > run ${ns.getScriptName()} getServers server-term home` +
         `\n     Example: > run ${ns.getScriptName()} --ns getPurchasedServers`
@@ -39,7 +36,6 @@ export function main(ns) {
     return;
   }
 
-  // Print result.
   ns.tprint(Array.isArray(result) ? `\n${result.join('\n')}` : result);
 }
 
@@ -49,11 +45,8 @@ export function main(ns) {
 export const lib = {
   /**
    * Returns an array of servers.
-   * @author excd
    * @remarks RAM cost: 0.2GB
    * @param {import('@ns').NS} ns - Netscript environment.
-   * @param {string} [term] - Search term for server names.
-   * @param {string} [hostname=home] - Hostname of server to scan.
    * @returns {string[]} Array of servers.
    */
   getServers: function (ns, term = '', hostname = 'home') {
@@ -67,10 +60,8 @@ export const lib = {
 export const nslib = {
   /**
    * Returns an array of purchased servers. Cheaper alternative to ns.getPurchasedServers().
-   * @author excd
    * @remarks RAM cost: 0GB
    * @param {import('@ns').NS} ns - Netscript environment.
-   * @param {string} [term=pserv] - Search term for server names.
    * @returns {string[]} Array of purchased servers.
    */
   _getPurchasedServers: function (ns, term = 'pserv') {
