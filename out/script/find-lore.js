fe49fd310ecfc2a38c9fed28f60460cfc729f9da
Excd <greggseamon@gmail.com>
2023-09-02 18:19:52 -0400

    Improve find-lore flag parsing

--- out/script/find-lore.js
+++ out/script/find-lore.js
@@ -11,20 +11,24 @@ import { get_lore, get_servers } from 'lib/server';
  * @param {import('@ns').NS} ns - Netscript environment.
  */
 export async function main(ns) {
-  if (ns.flags([['help', false]]).help) {
+  const flags = ns.flags([
+    ['help', false],
+    ['term', 'pserv'],
+  ]);
+
+  if (flags.help) {
     ns.tprint(
       'INFO: Copy lore from all servers up to an optionally specified search depth.' +
-        ' Automatically excludes purchased servers. Optionally exclude servers with a' +
+        ' Excludes purchased servers by default. Optionally exclude servers with a' +
         ' specified term instead.' +
-        `\n[Usage   /]> run ${ns.getScriptName()} <depth> <term>` +
-        `\n[Example /]> run ${ns.getScriptName()} 3`
+        `\n[Usage   /]> run ${ns.getScriptName()} <--term term> <depth>` +
+        `\n[Example /]> run ${ns.getScriptName()} --term my-term 3`
     );
     return;
   }
 
   // Arguments.
-  const depth = ns.args[0] || 1;
-  const excludeTerm = ns.args[1] || 'pserv';
+  const depth = (ns.args.length <= 1 ? ns.args[0] : ns.args[2]) || 1;
 
-  get_servers(ns, 'home', depth, excludeTerm).forEach((server) => get_lore(ns, server));
+  get_servers(ns, 'home', depth, flags.term).forEach((server) => get_lore(ns, server));
 }
